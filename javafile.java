a989c4de71219c6646406c2583e1c650ae7c597e
prathap <prathapreddyc3@gmail.com>
2022-03-03 17:45:50 +0530

    java

--- javafile.java
+++ javafile.java
@@ -1,2 +1,11 @@
 this is a java regestration page
+<<<<<<< Updated upstream
 this is my manager work
+=======
+1st day lkines code
+2nd day lines code
+3re day lines code
+90 per work completed
+100 per work completed
+
+>>>>>>> Stashed changes
